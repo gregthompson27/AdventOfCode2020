6d3852d8938d09ff049398ea4a2345e3c2471b13
Greg <gregthompson27@gmail.com>
2020-12-21 13:04:04 -0600

    (add) data from days 14 and 18

---
+++ Day10010/day18.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const data = fs.readFileSync('./Day10010/data.txt', 'utf-8').split('\n');
+
+console.log(data);
+
+// Part One - Before you can help with the homework, you need to understand it yourself. Evaluate the expression on each line of the homework; what is the sum of the resulting values?
+
+const partOne = () => {
+
+};
